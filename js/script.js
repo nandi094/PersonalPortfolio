4711a8405ddd93624c0f20ba08894db950b2ba29
nandi094 <nandigowda094@gmail.com>
2024-07-18 11:44:55 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-// JavaScript can be used to add interactivity if needed
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("JavaScript Loaded");
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector("nav ul");
+
+    if (menuToggle) {
+        menuToggle.addEventListener("click", () => {
+            navMenu.classList.toggle("show");
+        });
+    }
+
+    // Add smooth scrolling to anchor links
+    const links = document.querySelectorAll('nav ul li a');
+    for (const link of links) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const href = this.getAttribute("href");
+            const offsetTop = document.querySelector(href).offsetTop;
+
+            scroll({
+                top: offsetTop,
+                behavior: "smooth"
+            });
+        });
+    }
 });
